37c3bdbf869a73defaa89af7ded5fabd72209655
hiba-0325 <hibakm72@gmail.com>
2024-10-28 09:58:59 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import BlogList from "./Blog/BlogDetails";
-
 import HomePage from "./pages/home";
 import AddPost from "./Blog/newPost";
 import DetailsPage from "./Blog/BlogDetails";
