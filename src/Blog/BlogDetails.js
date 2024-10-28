8f6a7dea3908698842c7b627fb5821695a7fc9dd
hiba-0325 <hibakm72@gmail.com>
2024-10-28 11:47:23 +0530

    1

--- src/Blog/BlogDetails.js
+++ src/Blog/BlogDetails.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function DetailsPage() {
-  const blogDetails = useSelector((state) => state.posts.details);
+  const blogDetails = useSelector((state) => state.blog.blogDetails);
 
   return (
     <div>
